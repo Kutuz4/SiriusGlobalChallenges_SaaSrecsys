7620e45188a39db5fc9d727c214fe5dd73d1ba08
phantomgks <148693685+phantomgks@users.noreply.github.com>
2024-03-15 12:15:15 +0300

    Update ensembler.py

--- Backend/ensembler.py
+++ Backend/ensembler.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
+from flask_ngrok import run_with_ngrok
 import json
 import requests
+from flask import jsonify
+
 
 
 app = Flask(__name__)
+# run_with_ngrok(app) 
+
 
 class pr:
     def __init__(self, ban):
@@ -20,29 +25,35 @@ class Ensembler:
     def predict(self, data):   
         predicts = [x.predict(data) for x in self.a]
         return self.callable(predicts)
-        
-
-ensembler = Ensembler([pr(123)], lambda x: 1)
 
 
+class connector:
+    def __init__(self, url):
+        self.url = url 
+    def predict(self, user_id):
+        data = {'user_id': user_id}
+        response = requests.get(f"{self.url}/predict?user_id={user_id}").json()["prediction"]
+        return response
+
+connector = connector("https://basilisk-relaxed-ghost.ngrok-free.app/")
+ensembler = Ensembler([connector], lambda x: x[0])
+
+@app.route("/")
 @app.route('/predict', methods = ['POST', 'GET'])
 def answer():
     if request.method == 'POST':
-        data = request.json.post('data')
+        data = request.post('data')
         prediction = ensembler.predict(data)
-        print(1, prediction)
         return json.dumps({"prediction": prediction})
-    if request.method == 'GET':
-        user_id = request.args.get('user-id')
-        prediction = ensembler.predict(user_id)
+    if request.method == 'GET': 
+        user_id = request.args.get('user_id')
+        config = json.loads(user_id)
+        prediction = ensembler.predict(config)
         return json.dumps({"prediction": prediction})
+        
            
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-    
+    app.run()
 
 
     
