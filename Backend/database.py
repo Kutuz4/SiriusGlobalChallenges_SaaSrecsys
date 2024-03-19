4e141382e1f75a21de8dd3c5f2be623911a06a0d
bleedembaby <163101706+bleedembaby@users.noreply.github.com>
2024-03-19 16:09:44 +0300

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 import numpy as np
 import pandas as pd
@@ -10,7 +9,7 @@ import socket
 
 app = Flask(__name__)
 
-conn = sqlite3.connect("./krut.db")
+conn = sqlite3.connect("./krut.db", check_same_thread=False)
 cursor = conn.cursor()
 socket_url = "127.0.0.1"
 
@@ -21,9 +20,11 @@ with open("combined_data_1.txt", "r") as f:
             movie_id = int(line.strip(":\n"))
         else:
             user_id, rating, date = line.split(",")
-            if int(user_id) < 1000000:
+            if int(user_id) < 10000:
                 datat = (movie_id, user_id, rating, date.strip())
                 cursor.execute('INSERT INTO krut_movies (movie_id, user_id, rating, date) VALUES (?, ?, ?, ?);', datat)
+                
+
 @app.route('/adduser', methods=['GET', 'POST'])
 def adduser():
     if request.method == 'GET':
@@ -51,12 +52,12 @@ def send_file(file_name, server_socket):
     with open(file_name, 'rb') as file:
         for data in iter(lambda: file.read(1024), b''):
             server_socket.sendall(data)
-            
+    
 @app.route("/getdata", methods=["GET"])
 def getdata():
-    port = requests.args.get("port")
+    port = int(request.args.get("port"))
     host = socket_url
-    data_to_send = cursor.execute("SELECT * FROM krut_movies")
+    data_to_send = cursor.execute("SELECT * FROM krut_movies;")
     data = cursor.fetchall()
     row_count = 0 
     with open("data.csv", "w", newline="") as csv_file:
@@ -64,10 +65,17 @@ def getdata():
         for row in data:
             writer.writerow(row)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(host, port)
     client_socket.connect((host, port))
     send_file("data.csv", client_socket)
     client_socket.close()
-if __name__ == "__main__":
-    app.run()
+    
+    return "200"
 
+if __name__ == "__main__":
+    app.run(port=6000)
+
+#в get-запросе будет аргумент - port для соединения по сокету
+#необходимо передать по нему всю информацию из БД в формате csv/txt
+#строки таблицы разделяются \n а значение внутри одной строки запятой
 conn.close() 
