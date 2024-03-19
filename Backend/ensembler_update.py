997b5994cabf4eeee8658309f6aadb1e3d104fa3
phantomgks <148693685+phantomgks@users.noreply.github.com>
2024-03-19 17:17:43 +0300

    Update ensembler_update.py

--- Backend/ensembler_update.py
+++ Backend/ensembler_update.py
@@ -3,100 +3,122 @@ from flask_ngrok import run_with_ngrok
 import json
 import requests
 from flask import jsonify
+import numpy as np
 import socket
+import time
 
 
+def receive_file(client_socket, filell): 
+    with open(filell , 'wb') as file: 
+        while True: 
+            data = client_socket.recv(1024) 
+            if not data: 
+                break 
+            file.write(data)
+
+def send_file(server_socket, file_name):
+    with open(file_name, 'rb') as file:
+        for data in iter(lambda: file.read(1024), b''):
+            server_socket.sendall(data)
+            
+def delete_port(url):
+    return url.rstrip("1234567890").strip(":")
+
+def extract_port(url):
+    return int(url.split(":")[-1])
+            
 
 app = Flask(__name__)
 # run_with_ngrok(app) 
 
 
-data = {"url":["https://basilisk-relaxed-ghost.ngrok-free.app", "htttp:/localhost:8080"]}
+#data = {"url":["https://basilisk-relaxed-ghost.ngrok-free.app", "http://127.0.0.1:5000"], "database_url": "http://127.0.0.1:7000"}
+data = {"url":["127.0.0.1:24567"], "database_url": "127.0.0.1:6000"}
 
 with open('config.json', 'w') as outfile:
     json.dump(data, outfile)
 
 
 
-
 class Ensembler:
-    def __init__(self, a, callable):
+    def __init__(self, a, callable, database_url):
         self.a = a
         self.callable = callable      
+        self.database_url = database_url
+        
     def predict(self, data):   
         predicts = [x.predict(data) for x in self.a]
         return self.callable(predicts)
-    def fit(self, data):   
-        predicts = [x.predict(data) for x in self.a]
+    
+    def fit(self):  
+        for x in self.a:
+            x.fit()
+        
     def get_data(self):
-        def receive_file(client_socket, filell): 
-            with open(filell , 'wb') as file: 
-                while True: 
-                    data = client_socket.recv(1024) 
-                    if not data: 
-                        break 
-                    file.write(data)
-            # data_list = []
-            # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            #     s.connect(("127.0.0.1", 7000))
-            #     data = s.recv(1024)
-            #     data_list.append(data.decode())
-            # return data_list  
-        def main(): 
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-            host = '127.0.0.1' 
-            port = 5500
-            server_socket.bind((host, port)) 
-            server_socket.listen(50) 
-            while True: 
-                client_socket, addr = server_socket.accept() 
-                print(f"Connection from {addr}") 
-                file_name = "data.txt" 
-                print(f"Receiving file: {file_name}") 
-                receive_file(client_socket, file_name) 
-                print(f"File '{file_name}' received successfully.") 
-                client_socket.close() 
-                return file_name
-                
-        if __name__ == "__main__": 
-            main()
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        host = delete_port(self.database_url)
+        port = np.random.randint(low=20000, high=25000)
+        try:
+            requests.get(f"http://{self.database_url}/getdata?port={port}", timeout=0.1)
+        except:
+            pass
+        server_socket.bind((host, port))    
+        server_socket.listen(50)
+        
+        client_socket, addr = server_socket.accept() 
+        print(f"Connection from {addr}") 
+        file_name = "data.csv" 
+        print(f"Receiving file: {file_name}")
+        receive_file(client_socket, file_name)
+        print(f"File '{file_name}' received successfully.") 
+        client_socket.close() 
+        return file_name
   
 
 
-class connector:
-    def __init__(self, url):
-        self.url = url 
+class connector(Ensembler):
+    def __init__(self, url, data_file_name):
+        self.url = url
+        self.data_file_name = data_file_name
+        
     def predict(self, user_id):
         data = {'user_id': user_id}
         response = requests.get(f"{self.url}/predict?user_id={user_id}").json()["prediction"]
         return response
-    def fit(self, user_id):
-        data = {'user_id': user_id}
-        response = requests.get(f"{self.url}/predict?user_id={user_id}").json()["prediction"]
-        
+    
+    def fit(self): 
+        host = delete_port(self.url)
+        port = np.random.randint(low=20000, high=25000)
+        print(port)
+        print(self.url)
+        print(host)
+        try:
+            r = requests.get(f"http://{self.url}/fit?port={port}", timeout=0.1)
+        except:
+            pass
+        time.sleep(10)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        send_file(client_socket, "data.csv")
+        client_socket.close()
 
     
 with open('config.json', 'r') as file:
     config_json = json.load(file)
 
-
+data_file_name = "data.csv"
 connector_list = []
 for url in config_json['url']:
-    connector_instance = connector(url)  
+    connector_instance = connector(url, data_file_name) 
     connector_list.append(connector_instance)
 
 
-ensembler = Ensembler(connector_list, lambda x: x[0])
-
+ensembler = Ensembler(connector_list, lambda x: x[0], config_json["database_url"])
 ensembler.get_data()
+ensembler.fit()
 
 
-# request = requests.get("http://localhost:8080")
 
-# with open('data.csv', 'r') as dat:
-#     file_data = dat.read()
-# for url in connector_list:
-#     response = requests.post(url, data=file_data)
 
 
 @app.route('/predict', methods = ['POST', 'GET'])
@@ -113,4 +135,4 @@ def answer():
         
            
 if __name__ == "__main__":
-    app.run(host = '127.0.0.1', port = 5600)
+    app.run(port = 7000)
